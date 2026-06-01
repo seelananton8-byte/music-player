4835fe2da045e0d1198794c271d0b90fad5edb45
seelananton8-byte <seelananton8@gmail.com>
2026-06-01 12:05:09 +0530

    Add Some Songs

--- src/data/songs.js
+++ src/data/songs.js
@@ -2,7 +2,7 @@ const songs = [
   {
     id: 1,
     title: "Kodi Kodi Minnalgal",
-    movie: "Tamil Hits",
+    movie: "Jeyam",
     image:
       "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -11,7 +11,7 @@ const songs = [
   {
     id: 2,
     title: "Kavithayae Theriyuma",
-    movie: "Tamil Melody",
+    movie: "Jeyam",
     image:
       "https://images.unsplash.com/photo-1511379938547-c1f69419868d?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -20,7 +20,7 @@ const songs = [
   {
     id: 3,
     title: "Thodu Vaanam",
-    movie: "StarMusiQ",
+    movie: "Anegan",
     image:
       "https://images.unsplash.com/photo-1501386761578-eac5c94b800a?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -29,7 +29,7 @@ const songs = [
   {
     id: 4,
     title: "Roja Kadale",
-    movie: "Romantic Tamil",
+    movie: "Anegan",
     image:
       "https://images.unsplash.com/photo-1499364615650-ec38552f4f34?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -38,7 +38,7 @@ const songs = [
   {
     id: 5,
     title: "Danga Maari Oodhari",
-    movie: "Anirudh",
+    movie: "Anegan",
     image:
       "https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -47,7 +47,7 @@ const songs = [
   {
     id: 6,
     title: "Aathadi Aathadi",
-    movie: "Tamil Love",
+    movie: "Anegan",
     image:
       "https://images.unsplash.com/photo-1459749411175-04bf5292ceea?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -56,7 +56,7 @@ const songs = [
   {
     id: 7,
     title: "Entha Thesathil",
-    movie: "Tamil Love",
+    movie: "Priyamana Thozhi",
     image: 
     "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -65,7 +65,7 @@ const songs = [
   {
     id: 8,
     title: "Kattre-Poongattre",
-    movie: "Tamil Love",
+    movie: "Priyamana Thozhi",
     image:
     "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -74,7 +74,7 @@ const songs = [
   {
     id: 9,
     title: "Maan Kuttiye",
-    movie: "Tamil Love",
+    movie: "Priyamana Thozhi",
     image:
     "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1200&auto=format&fit=crop",
     audio:
@@ -83,7 +83,7 @@ const songs = [
   {
     id: 10,
     title: "Penne Neeyum",
-    movie: "Tamil Love",
+    movie: "Priyamana Thozhi",
     image:
     "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1200&auto=format&fit=crop",
     audio:
